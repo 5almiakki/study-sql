44b1818d7047c1642a8a564b3d5e7ca7c4ca6fbd
5almiakki <jpui9635@gmail.com>
2026-01-21 17:56:09 +0900

    solvesql count-stamps 스탬프를 찍어드려요 풀이

---
+++ solvesql/20260121_count_stamps_스탬프를_찍어드려요.sql
@@ -0,0 +1,14 @@
+SELECT
+  stamp,
+  COUNT(*) AS count_bill
+FROM (
+  SELECT
+    CASE
+      WHEN total_bill >= 25 THEN 2
+      WHEN total_bill >= 15 THEN 1
+      ELSE 0
+    END AS stamp
+  FROM tips
+) AS t
+GROUP BY stamp
+ORDER BY stamp ASC;
